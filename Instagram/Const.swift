3c367c0343893bc7b780bc27d43f40ebd966986e
hiroki.watanabe <test@example.com>
2021-10-01 19:14:03 +0900

    コメント機能を実装

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by Hiroki Watanabe on 2021/09/21.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
